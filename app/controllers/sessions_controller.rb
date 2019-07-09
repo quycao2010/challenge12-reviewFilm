49726752c0f21d856f5576fde744148e9c41bee2
quycao2010 <quycao2010@gmail.com>
2019-07-09 16:11:54 +0700

    Fix redirect url when signup complete

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -8,7 +8,7 @@ class SessionsController < ApplicationController
     user = User.find_by(email: params[:session][:email])
     if user && user.authenticate(params[:session][:password])
       log_in user
-      redirect_to root_path
+      redirect_to login_path
     else
       render 'new'
     end
